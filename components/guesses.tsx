fb1f4aa79aa3cd852536a5fdc93000411ac5aa8e
Kamil Zurek <theoriginalpole@gmail.com>
2022-11-10 23:41:54 +0000

    Displaying guesses with details

--- components/guesses.tsx
+++ components/guesses.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 
 interface GuessesProps {
   guesses: Champ[];
+  champToGuess: Champ | undefined;
 }
 
-const Guesses = ({ guesses }: GuessesProps) => {
+const Guesses = ({ guesses, champToGuess }: GuessesProps) => {
   return (
     <>
-      {guesses.length > 0 && (
+      {champToGuess && guesses.length > 0 && (
         <div className="flex justify-center mt-4 space-x-4 select-none">
           {/* NAME */}
           <div className="flex flex-col w-20 items-center">
@@ -36,9 +37,32 @@ const Guesses = ({ guesses }: GuessesProps) => {
             <div className="border-t-[1px] w-[80%] mt-2" />
             {guesses.map((guess, index) => (
               <div
-                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                className={`flex relative w-20 h-20 border mt-2 items-center justify-center ${
+                  guess.set === champToGuess.set
+                    ? "bg-green-700 hover:bg-green-500"
+                    : "bg-red-700 hover:bg-red-500"
+                }`}
                 key={`${guess.name}, ${index}`}
               >
+                {guess.set < champToGuess.set ? (
+                  <Image
+                    src="/icons/up_arrow.png"
+                    alt="arrow"
+                    width="64"
+                    height="64"
+                    className="absolute opacity-40"
+                  />
+                ) : (
+                  guess.set > champToGuess.set && (
+                    <Image
+                      src="/icons/up_arrow.png"
+                      alt="arrow"
+                      width="64"
+                      height="64"
+                      className="absolute opacity-40 rotate-180"
+                    />
+                  )
+                )}
                 <p className="z-10">{guess.set.replace("-", ".")}</p>
               </div>
             ))}
@@ -53,9 +77,32 @@ const Guesses = ({ guesses }: GuessesProps) => {
             <div className="border-t-[1px] w-[80%] mt-2" />
             {guesses.map((guess, index) => (
               <div
-                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                className={`flex relative w-20 h-20 border mt-2 items-center justify-center ${
+                  guess.cost === champToGuess.cost
+                    ? "bg-green-700 hover:bg-green-500"
+                    : "bg-red-700 hover:bg-red-500"
+                }`}
                 key={`${guess.name}, ${index}`}
               >
+                {guess.cost < champToGuess.cost ? (
+                  <Image
+                    src="/icons/up_arrow.png"
+                    alt="arrow"
+                    width="64"
+                    height="64"
+                    className="absolute opacity-40"
+                  />
+                ) : (
+                  guess.cost > champToGuess.cost && (
+                    <Image
+                      src="/icons/up_arrow.png"
+                      alt="arrow"
+                      width="64"
+                      height="64"
+                      className="absolute opacity-40 rotate-180"
+                    />
+                  )
+                )}
                 <p className="z-10">{guess.cost}</p>
               </div>
             ))}
@@ -67,9 +114,35 @@ const Guesses = ({ guesses }: GuessesProps) => {
             <div className="border-t-[1px] w-[80%] mt-2" />
             {guesses.map((guess, index) => (
               <div
-                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                className={`flex relativ w-20 h-20 border mt-2 items-center justify-center ${
+                  guess.health.split("/")[0].trim() ===
+                  champToGuess.health.split("/")[0].trim()
+                    ? "bg-green-700 hover:bg-green-500"
+                    : "bg-red-700 hover:bg-red-500"
+                }`}
                 key={`${guess.name}, ${index}`}
               >
+                {guess.health.split("/")[0].trim() <
+                champToGuess.health.split("/")[0].trim() ? (
+                  <Image
+                    src="/icons/up_arrow.png"
+                    alt="arrow"
+                    width="72"
+                    height="72"
+                    className="absolute opacity-40"
+                  />
+                ) : (
+                  guess.health.split("/")[0].trim() >
+                    champToGuess.health.split("/")[0].trim() && (
+                    <Image
+                      src="/icons/up_arrow.png"
+                      alt="arrow"
+                      width="64"
+                      height="64"
+                      className="absolute opacity-40 rotate-180"
+                    />
+                  )
+                )}
                 <p className="z-10">{guess.health.split("/")[0].trim()}</p>
               </div>
             ))}
@@ -81,9 +154,32 @@ const Guesses = ({ guesses }: GuessesProps) => {
             <div className="border-t-[1px] w-[80%] mt-2" />
             {guesses.map((guess, index) => (
               <div
-                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                className={`flex relative w-20 h-20 border mt-2 items-center justify-center ${
+                  guess.range === champToGuess.range
+                    ? "bg-green-700 hover:bg-green-500"
+                    : "bg-red-700 hover:bg-red-500"
+                }`}
                 key={`${guess.name}, ${index}`}
               >
+                {guess.range < champToGuess.range ? (
+                  <Image
+                    src="/icons/up_arrow.png"
+                    alt="arrow"
+                    width="72"
+                    height="72"
+                    className="absolute opacity-40"
+                  />
+                ) : (
+                  guess.range > champToGuess.range && (
+                    <Image
+                      src="/icons/up_arrow.png"
+                      alt="arrow"
+                      width="64"
+                      height="64"
+                      className="absolute opacity-40 rotate-180"
+                    />
+                  )
+                )}
                 <p className="z-10">{guess.range}</p>
               </div>
             ))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,17 @@ import Head from "next/head";
 import Image from "next/image";
 import Searchbox from "../components/searchbox";
 import type { Champ } from "./types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Guesses from "../components/guesses";
+import champs from "../json/all_champs.json";
 
 export default function Home() {
   const [guesses, setGuesses] = useState<Champ[]>([]);
+  const [champToGuess, setChampToGuess] = useState<Champ | undefined>();
+
+  useEffect(() => {
+    setChampToGuess(champs[Math.floor(Math.random() * champs.length)]);
+  }, []);
 
   const handleGuess = (champ: Champ) => {
     setGuesses([champ, ...guesses]);
@@ -26,7 +32,7 @@ export default function Home() {
       <div className="flex flex-col items-center">
         <Searchbox handleGuess={handleGuess} />
       </div>
-      <Guesses guesses={guesses} />
+      <Guesses guesses={guesses} champToGuess={champToGuess} />
     </div>
   );
 }
