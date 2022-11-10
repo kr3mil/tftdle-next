d46490ef5b80b71402563fd45437fffaa91171ea
Kamil Zurek <theoriginalpole@gmail.com>
2022-11-10 23:25:45 +0000

    Displaying more properties

--- components/guesses.tsx
+++ components/guesses.tsx
@@ -11,6 +11,7 @@ const Guesses = ({ guesses }: GuessesProps) => {
     <>
       {guesses.length > 0 && (
         <div className="flex justify-center mt-4 space-x-4 select-none">
+          {/* NAME */}
           <div className="flex flex-col w-20 items-center">
             <p className="font-semibold">Champion</p>
             <div className="border-t-[1px] w-[80%] mt-2" />
@@ -25,6 +26,65 @@ const Guesses = ({ guesses }: GuessesProps) => {
                   width="128"
                   height="128"
                 />
+              </div>
+            ))}
+          </div>
+
+          {/* SET */}
+          <div className="flex flex-col w-20 items-center">
+            <p className="font-semibold">Set</p>
+            <div className="border-t-[1px] w-[80%] mt-2" />
+            {guesses.map((guess, index) => (
+              <div
+                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                key={`${guess.name}, ${index}`}
+              >
+                <p className="z-10">{guess.set.replace("-", ".")}</p>
+              </div>
+            ))}
+          </div>
+
+          {/* TRAITS */}
+          {/* TODO */}
+
+          {/* COST */}
+          <div className="flex flex-col w-20 items-center">
+            <p className="font-semibold">Cost</p>
+            <div className="border-t-[1px] w-[80%] mt-2" />
+            {guesses.map((guess, index) => (
+              <div
+                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                key={`${guess.name}, ${index}`}
+              >
+                <p className="z-10">{guess.cost}</p>
+              </div>
+            ))}
+          </div>
+
+          {/* HEALTH */}
+          <div className="flex flex-col w-20 items-center">
+            <p className="font-semibold">Health</p>
+            <div className="border-t-[1px] w-[80%] mt-2" />
+            {guesses.map((guess, index) => (
+              <div
+                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                key={`${guess.name}, ${index}`}
+              >
+                <p className="z-10">{guess.health.split("/")[0].trim()}</p>
+              </div>
+            ))}
+          </div>
+
+          {/* RANGE */}
+          <div className="flex flex-col w-20 items-center">
+            <p className="font-semibold">Range</p>
+            <div className="border-t-[1px] w-[80%] mt-2" />
+            {guesses.map((guess, index) => (
+              <div
+                className={`flex w-20 h-20 border mt-2 items-center justify-center`}
+                key={`${guess.name}, ${index}`}
+              >
+                <p className="z-10">{guess.range}</p>
               </div>
             ))}
           </div>
