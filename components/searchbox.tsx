3a3d6211ac3e6bd37a96be128ece4a354a8cbdcf
Kamil Zurek <kamil.zurek@siemens.com>
2022-11-10 11:50:34 +0000

    Better search selection

--- components/searchbox.tsx
+++ components/searchbox.tsx
@@ -5,12 +5,12 @@ import champs from "../json/all_champs.json";
 import Image from "next/image";
 
 const Searchbox = () => {
-  const [selectedPerson, setSelectedPerson] = useState(undefined);
+  const [selectedPerson, setSelectedPerson] = useState("");
   const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    console.log(champs);
-  }, []);
+    console.log(selectedPerson);
+  }, [selectedPerson]);
 
   const filteredChamps =
     searchText === ""
@@ -23,16 +23,19 @@ const Searchbox = () => {
     <div className="relative cursor-default overflow-hidden rounded-lg text-left w-[42rem] shadow-md focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
       <Combobox value={selectedPerson} onChange={setSelectedPerson}>
         <Combobox.Input
-          placeholder="Type champion name..."
           onChange={(event) => setSearchText(event.target.value)}
           className="border-none py-4 pl-3 pr-10 w-full text-lg leading-5 focus:ring-0 focus:outline-none"
         />
-        <Combobox.Options className="max-h-[40rem] overflow-y-scroll">
+        <Combobox.Options className={`max-h-[40rem] overflow-y-scroll`}>
           {filteredChamps.map((champ) => (
             <Combobox.Option
               key={`${champ.name} - ${champ.set}`}
               value={champ}
-              className="flex space-x-2 items-center last:border-b-0 border-b-[1px] p-2 hover:bg-blue-300 hover:cursor-pointer"
+              className={({ active }) =>
+                `flex space-x-2 items-center last:border-b-0 border-b-[1px] p-2 hover:bg-blue-300 hover:cursor-pointer ${
+                  active ? "bg-teal-600 text-white" : ""
+                }`
+              }
             >
               <Image
                 src={`/${champ.icon}`}
