6efddab2f95f1d9407d9b50240710007555ae3a2
Kamil Zurek <kamil.zurek@siemens.com>
2022-11-15 08:53:37 +0000

    Champ based on todays date

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,23 @@ export default function Home() {
   const [champToGuess, setChampToGuess] = useState<Champ | undefined>();
   const [guessedCorrectly, setGuessedCorrectly] = useState<Boolean>(false);
 
+  function mulberry32(a: number) {
+    return function () {
+      var t = (a += 0x6d2b79f5);
+      t = Math.imul(t ^ (t >>> 15), t | 1);
+      t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+      return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+    };
+  }
+
   useEffect(() => {
-    setChampToGuess(champs[Math.floor(Math.random() * champs.length)]);
+    let today = new Date().toLocaleDateString();
+    let randomSeed: number = parseInt(today.replace("/", ""));
+
+    let randomNumFunc: Function = mulberry32(randomSeed);
+    let randomNum = randomNumFunc();
+
+    setChampToGuess(champs[Math.floor(randomNum * champs.length)]);
   }, []);
 
   useEffect(() => {
